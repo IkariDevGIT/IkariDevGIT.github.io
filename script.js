2ddc776e06dd80cb665aca1585a4110087528575
IkariDevGIT <ikari.devloper@gmail.com>
2023-10-31 05:00:07 +0100

    wtf

--- script.js
+++ script.js
@@ -93,8 +93,28 @@ function openPostOnly(e) {
     seePostOnly(id);
 }
 
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
 
 function showRandomImage() {
+    if(!document.cookie == ""){
+        username = getCookie("username")
+        document.getElementById("img_html-chan").style.display = "block";
+        return;
+    }
     const randomValue = Math.random(); // Generate a random number between 0 and 1
     if (randomValue < 0.05) {
       document.getElementById("img_ikari-cringe").style.display = "block"; // 10% chance to show image 1
@@ -195,9 +215,41 @@ function openWindow(window, addinfo){
     }
 }
 
+function html_chan_love(level1){
+    if (level1 === undefined){ return parseInt(getCookie("love")); }
+
+    document.cookie = "love=" + level1;
+}
+
+function html_chan(window1){
+    alert("Soo.. hey, im html-chan.")
+    username = prompt("Whats your name?")
+    if (!username == "") {
+        alert("O-okay.. " + username + " sounds pretty nice..")
+        if (prompt("You still wanna chat with me?\nyes/no").toLowerCase() == "yes") {
+            alert("Thank you again " + username + ", for.. not leaving me now...")
+            alert("I guess i'll be here from now on then!")
+            document.cookie = "username=" + username + ";love=10";
+            document.cookie = "love=10";
+            window.location.reload();
+        }else{
+            alert("Ohh.. like always...")
+            window1.close();
+        }
+    }else{ alert("Okay then.."); window1.close(); }
+}
+
+var htmlchan_event_active = false;
+
 window.addEventListener('load', function() {
+    //if(!document.cookie == ""){
+    //    username = getCookie("username")
+    //    alert("Hey, " + username)
+    //}
+
     const randomValue = Math.random(); // Generate a random number between 0 and 1
-    if (randomValue < 0.01) {
+    if (randomValue < 0.91 && document.cookie == "") {
+        //// Html-chan
         ajaxwin=dhtmlwindow.open('what_is_this', 'inline',
             `
             <h3>Huh, what is this?</h3>
@@ -205,13 +257,15 @@ window.addEventListener('load', function() {
             <p>It feels weird being here again..</p>
             `, 'Huh.. what is this?', 'width=650px,height=400px,left=300px,top=100px,resize=0,scrolling=1');
         ajaxwin.onclose=function(){
+            if(htmlchan_event_active == true){ return true; }
+            htmlchan_event_active = true
             alert("Why?");
             alert("...");
             if (prompt("Did you enjoy my short company?\nyes/no").toLowerCase() == "yes") {
                 alert("Ohh..")
                 if (prompt("Could i stay maybe?\nyes/no").toLowerCase() == "yes") {
                     alert("thanks..")
-                    ajaxwin.load("inline", "<p>I don't know what to say..</p><p>Just.. thanks!</p>", "thanks..")
+                    html_chan(ajaxwin);
                     return false;
                 }else{
                     alert("Okay then..")
@@ -252,6 +306,7 @@ window.addEventListener('load', function() {
         switchTo(0);
     }
     document.getElementById("loading-screen").style.display = "none";
+    
 })
 
 /**
@@ -311,6 +366,111 @@ function switchToBlog(blogid2, blogonly2) {
             seePostOnly(blogid2)
         }
     }
+}
+
+function html_chan_main() {
+    // Build in love system / variable
+    username = getCookie("username")
+    var num = Math.floor(Math.random() * 7) + 1;
+    switch (num){
+        case 1:
+            alert(`${username}! look what i made!`)
+            ajaxwin=dhtmlwindow.open('htmlchan-window-poem', 'inline',
+            `
+            <p>In the world of code, where websites come alive,<br>
+            HTML's the language that helps them thrive.<br>
+            With tags and elements, it structures the page,<br>
+            Content and design, like actors on a stage.<br>
+            <Head> and <body>, the document's core,<br>
+            Links and images, and so much more.<br>
+            A language of structure, it's the web's backbone,<br>
+            HTML, the builder, on a digital stone.<br>
+            <br><br>
+            It links us all in this digital sphere,<br>
+            Invisible code, yet it's crystal clear.<br>
+            HTML, the language that connects us all,<br>
+            In the boundless web, we stand tall.</p>
+
+            <h3>Cool huh?</h3>
+            `, 'html-chan\'s poem', 'width=700px,height=400px,left=300px,top=100px,resize=0,scrolling=1');
+            break;
+        case 2:
+            alert(`${username}! That tickles!`)
+            html_chan_love(html_chan_love(undefined)+1);
+            break;
+        case 3:
+            alert(`${username}.. we need to talk..`)
+            if (!prompt("\nyes/no").toLowerCase() == "yes") {
+                alert("Okay..")
+                html_chan_love(html_chan_love(undefined)-1);
+                break;
+            }
+            alert(`Thanks ${username}, i've been thinking about us.. and this website.`)
+            alert("You know.. it could go down every time and we would never meet again..")
+            alert("I don't want that to happen! Do you promise you will be with me?")
+            if (!prompt("\nyes/no").toLowerCase() == "yes") {
+                alert("Wow.. i didn't think you would be that harsh..")
+                html_chan_love(html_chan_love(undefined)-2);
+                break;
+            }
+            alert("Thanks..")
+            html_chan_love(html_chan_love(undefined)+3);
+            break;
+        case 4:
+            alert("Hey handsome..")
+            html_chan_love(html_chan_love(undefined)+1);
+            break;
+        case 5:
+            alert(`${username}, please give me your social security number.`)
+            alert("Just joking haha")
+            html_chan_love(html_chan_love(undefined)+1);
+            break;
+        case 6:
+            alert("Do you AI RP?")
+            if (!prompt("\nyes/no").toLowerCase() == "yes") {
+                alert("Okay..")
+                alert("boring")
+                html_chan_love(html_chan_love(undefined)-1);
+                break;
+            }
+            alert(`Nice ${username}, hopefully not some weird stuff?`)
+            if (!prompt("\nweird/no").toLowerCase() == "weird") {
+                alert("How do i just know you are lying?")
+                html_chan_love(html_chan_love(undefined)-1);
+                break;
+            }
+            alert("At least you are honest with me :)")
+            alert("So i don't mind")
+            html_chan_love(html_chan_love(undefined)+3);
+            break;
+        case 7:
+            prmt = prompt("[Talk to her, options: \nschmex?\ndo you like html?]").toLowerCase()
+            switch (prmt){
+                case "schmex?":
+                    if(html_chan_love(undefined) >= 25){
+                        alert("[Love level success!]")
+                        alert("You really wanna do this.. huh?")
+                        alert("Okay then..")
+                        alert("[From ikaridev: what the fuck am i doing, and why am i doing it]")
+                        alert("Puhh.. that was... nice")
+                        alert("Thanks.. " + username)
+                        html_chan_love(html_chan_love(undefined)+5);
+                        break;
+                    }else{
+                        alert("[Love level too low]")
+                        alert("H-HUHH? N-N-noo way...")
+                        html_chan_love(html_chan_love(undefined)-2);
+                        break;
+                    }
+                case "do you like html?":
+                    alert("Yes.. well duh")
+                    html_chan_love(html_chan_love(undefined)+1);
+                    break;
+            }
+            break;
+
+    }
+
 }
 
 
